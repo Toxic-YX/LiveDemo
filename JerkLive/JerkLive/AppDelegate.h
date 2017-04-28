47c613e3b06fe60e9e37767de7a4c44ef3bfb3ac
nanjingxijian <xiang.yu@mhealth365.com>
2017-04-28 10:46:36 +0800

    Add Demo and Framework

---
+++ JerkLive/JerkLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JerkLive
+//
+//  Created by YuXiang on 2017/4/27.
+//  Copyright © 2017年 Rookie.YXiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JerkLive/JerkLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JerkLive
+//
+//  Created by YuXiang on 2017/4/27.
+//  Copyright © 2017年 Rookie.YXiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
